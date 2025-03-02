885beb4369c3c3543075215af79c704921636dfb
Zach Novak <Znovak500@gmail.com>
2025-03-02 03:46:00 -0600

    final

--- Patera/Assets/PateraAssets/Scripts/checkFallen.cs
+++ Patera/Assets/PateraAssets/Scripts/checkFallen.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class checkFallen : MonoBehaviour
 {
     public float tippingThreshold = 15.0f;
-
+    public AudioClip soundFx;
     // Update is called once per frame
+
     void Update()
     {   
         Rigidbody rb = GetComponent<Rigidbody>();
+        AudioSource box = GameObject.FindWithTag("Stereo").GetComponent<AudioSource>();
         float angle = Vector3.Angle(rb.transform.up, Vector3.up);
-        
+        soundFx = box.clip;
+
         if(angle > tippingThreshold)
         {
+            Debug.Log(angle.ToString());
             GameManager.score += 10;
+            box.PlayOneShot(soundFx);
             Destroy(gameObject);
         }
      }
